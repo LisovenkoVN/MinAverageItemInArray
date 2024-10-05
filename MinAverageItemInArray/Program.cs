a1f98ea04f84d755206d8994bfd31d4e0ccebd42
thatf <thatf@BOYNEXTDOOR>
2024-10-05 22:58:25 +1000

    Добавьте файлы проекта.

---
+++ MinAverageItemInArray/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MinAverageItemInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Min, Avrg;
+            int[] NewArray = { 1, 2, 3, 50, 99 };
+
+            Avrg = (int)NewArray.Average();
+            Min = NewArray[0];
+            foreach (int item in NewArray)
+            {
+                if (item < Min) Min = item;
+            }
+
+            var str = string.Join(" ", NewArray);
+            Console.Write($"Массив: {str}");
+            Console.WriteLine("\n");
+            Console.WriteLine($"Минимальное: {Min.ToString()}, Среднее: {Avrg.ToString()}");
+        }
+    }
+}
